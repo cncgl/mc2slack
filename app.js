2196468dd4489b9763a15b5422d8bc8e9bd2201f
Shigeru Kondou <concigel@gmail.com>
2015-07-04 02:16:35 +0900

    init

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-var config = require('./config');
-var request = require('request');
-var MilkCocoa = require('./milkcocoa');
-var milkcocoa = new MilkCocoa('https://'+ config.appid +'.mlkcca.com');
+var config    = require('./config');
+var request   = require('request');
+var MilkCocoa = require('milkcocoa');
+var milkcocoa = new MilkCocoa( config.appid +'.mlkcca.com');
 var ds = milkcocoa.dataStore(config.ds);
 
 ds.on('push', function(data) {
   console.log(data);
-    var mes = '「' + data.value.name + '('+ data.value.email +')」さんから問い合わせがありました。';
-    mes += '[' + data.value.title + ']';
-    mes += data.value.body;
 
-    var options = {
-        url: config.slack.url,
-        form: 'payload={"channel": "'+config.slack.channel+'", "username": "'+config.slack.username+'", "text": "'+ mes +'"}',
-        json: true
-    };
+  var mes = '「' + data.value.name + '('+ data.value.mail +')」さんから問い合わせがありました。';
+  mes += data.value.msg;
 
-    request.post(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-           console.log(body);
-        }else{
-            console.log('error: '+ response.statusCode);
-        }
-    });
+  var options = {
+    url: config.slack.url,
+    form: 'payload={"channel": "'+config.slack.channel+'", "username": "'+config.slack.username+'", "text": "'+ mes +'"}',
+    json: true
+  };
 
+  request.post(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }else{
+      console.log('error: '+ response.statusCode);
+    }
+  });
 });
